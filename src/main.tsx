613a1ad0e36833b71508d3c3e67535a2be0cd98f
SalehZababneh <saleh.zabadneh@gmail.com>
2024-11-28 18:07:57 +0300

    try to fix ts and es linkt v8

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import { Toaster } from "@/components/ui/toaster";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ReactQueryProvider } from "./providers/react-query.tsx";
+import { ReactQueryProvider } from "./providers/react-query";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
