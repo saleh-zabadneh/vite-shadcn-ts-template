8dcb7ebde0f8ec5ab88387f7fe05c526799b6e66
SalehZababneh <saleh.zabadneh@gmail.com>
2024-11-28 18:45:17 +0300

    try to fix ts and es linkt v11

--- eslint.config.js
+++ eslint.config.js
@@ -3,35 +3,19 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import prettier from "eslint-config-prettier";
-import prettierPlugin from "eslint-plugin-prettier";
 
 export default tseslint.config(
   { ignores: ["dist"] },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...reactHooks.configs.recommended,
-  prettier,
   {
-    files: ["**/*.{ts,tsx,js,jsx}"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.es2021,
-      },
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        project: "./tsconfig.json",
-      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
     plugins: {
-      "@typescript-eslint": tseslint.plugin,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      prettier: prettierPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -46,13 +30,5 @@ export default tseslint.config(
       "@typescript-eslint/explicit-module-boundary-types": "off",
       "@typescript-eslint/no-explicit-any": "warn",
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
-  {
-    ignores: ["dist/**"],
   }
 );
